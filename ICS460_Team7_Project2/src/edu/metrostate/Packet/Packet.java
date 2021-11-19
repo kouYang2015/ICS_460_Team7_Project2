878332ae374b6008299293baab0613cb36d6af58
Kou <Kou@KY>
2021-11-18 21:01:10 -0600

    Packet class created

---
+++ ICS460_Team7_Project2/src/edu/metrostate/Packet/Packet.java
@@ -0,0 +1,42 @@
+package edu.metrostate.Packet;
+
+public class Packet {
+	short cksum; //16-bit 2-byte 
+	short len;	//16-bit 2-byte 
+	int ackno;	//32-bit 4-byte 
+	int seqno ; //32-bit 4-byte Data packet Only 
+	byte[] data = new byte[500]; //0-500 bytes. Data packet only. Variable
+	
+	public short getCksum() {
+		return cksum;
+	}
+	public void setCksum(short cksum) {
+		this.cksum = cksum;
+	}
+	public short getLen() {
+		return len;
+	}
+	public void setLen(short len) {
+		this.len = len;
+	}
+	public int getAckno() {
+		return ackno;
+	}
+	public void setAckno(int ackno) {
+		this.ackno = ackno;
+	}
+	public int getSeqno() {
+		return seqno;
+	}
+	public void setSeqno(int seqno) {
+		this.seqno = seqno;
+	}
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	
+	
+}
