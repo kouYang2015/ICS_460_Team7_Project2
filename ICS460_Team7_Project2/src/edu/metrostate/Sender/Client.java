91e26469e5165b7e9001d73660d2156d598f52f3
Kou <Kou@KY>
2021-12-01 02:57:01 -0600

    sendPacket updated. Now reflects timeout, sending packet, receive ackP

--- ICS460_Team7_Project2/src/edu/metrostate/Sender/Client.java
+++ ICS460_Team7_Project2/src/edu/metrostate/Sender/Client.java
@@ -16,52 +16,58 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Client {
-	private final int DEFAULT_PORT = 12345;
-	int port;
+	private static final int DEFAULT_PORT = 12345;
+	private static final int DEFAULT_PACKET_SIZE = 200;
+	private static final int DEFAULT_TIMEOUT = 200;
+	private static final double DEFAULT_CORRUPTCHANCE = 0;
+	private int port;
+	private int packetSize; //command line argument for packet size
+	private int timeout; //timeout interval to know when to re-send packets
+	private double corruptChance; //chance to corrupt the datagrams to send
 	private DatagramSocket datagramSocket;
 	private InetAddress inetAddress;
 	private int startOffset = 0;	//offset byte counter. Dynamic
 	private int seqnoCounter = 1;
-	private int acknoCounter = 1;
-	private File outputFile;	//The file we want to send. May not need
 	private byte[] fileContent;	//total number of bytes in file
-	private int packetSize; //command line argument for packet size
-	private int timeout; //timeout interval to know when to re-send packets
-	private double corruptchance; //chance to corrupt the datagrams to send
-	private final int DEFAULT_PACKET_SIZE = 200;
-	private final int DEFAULT_TIMEOUT = 200;
-	private final double DEFAULT_CORRUPTCHANCE = 0;
 	
-	public Client(DatagramSocket datagramSocket, InetAddress inetAddress, int packetSize, int timeout, double corruptchance, int port) {
+	
+	public Client(InetAddress inetAddress, int packetSize, int timeout, double corruptchance, int port) throws SocketException {
 		super();
-		this.datagramSocket = datagramSocket;
-		this.inetAddress = inetAddress;
-		this.packetSize = packetSize;
-		if (packetSize == -1) {
-			packetSize = DEFAULT_PACKET_SIZE;
-		} else if (packetSize > 500) {
-			packetSize = 500;
+		try {
+			this.inetAddress = (inetAddress == null ? inetAddress = InetAddress.getLocalHost() : inetAddress);
+		} catch (UnknownHostException e1) {
+			System.out.println("UnknownHost.");
 		}
-		if (timeout == -1) {
-			this.timeout = DEFAULT_TIMEOUT;
-		} else {
-			this.timeout = timeout;
-		}
-		if (corruptchance == -1) {
-			this.corruptchance = DEFAULT_CORRUPTCHANCE;
-		} else {
-			this.corruptchance = corruptchance;
-		}
-		if (inetAddress == null) {
-			try {
-				inetAddress = InetAddress.getLocalHost();
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
-		}
-		if (port == -1) {
-			this.port = DEFAULT_PORT;
-		}
+		this.packetSize = (packetSize == -1 ? DEFAULT_PACKET_SIZE : packetSize > 500 ? 500 : packetSize);
+		this.timeout = (timeout == -1 ? DEFAULT_TIMEOUT : timeout);
+		this.corruptChance = (corruptchance == -1 ? DEFAULT_CORRUPTCHANCE : corruptchance);
+		this.port = (port == -1 ? DEFAULT_PORT : port);
+		this.datagramSocket = new DatagramSocket(port);
+//		if (packetSize == -1) {
+//			packetSize = DEFAULT_PACKET_SIZE;
+//		} else if (packetSize > 500) {
+//			packetSize = 500;
+//		}
+//		if (timeout == -1) {
+//			this.timeout = DEFAULT_TIMEOUT;
+//		} else {
+//			this.timeout = timeout;
+//		}
+//		if (corruptchance == -1) {
+//			this.corruptChance = DEFAULT_CORRUPTCHANCE;
+//		} else {
+//			this.corruptChance = corruptchance;
+//		}
+//		if (inetAddress == null) {
+//			try {
+//				inetAddress = InetAddress.getLocalHost();
+//			} catch (UnknownHostException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		if (port == -1) {
+//			this.port = DEFAULT_PORT;
+//		}
 	}
 	
 	/**
@@ -77,27 +83,52 @@ public class Client {
 	}
 
 	/**
-	 * Method used to send a requestPacket from Client to Server. 
+	 * Method used to send a requestPacket from Client to Server.
+	 * @throws IOException 
+	 * 
+	 * @throws SocketException
 	 */
-	public void sendPacket() {
-		//Buffer length decides how many packets we send. If buffer is larger = less #packets. Smaller = more #packets.
-		System.out.println("Content Length:" + fileContent.length + "\nbuffer Length: " + packetSize); //TODO: DEBUG STATEMENT DELETE AFTER
-		for (int i = 0; i < Math.floor(fileContent.length/packetSize); i++) {
+	public void sendPacket() throws IOException {
+		boolean timedOut = false; //Use this boolean to determine if timed out? Resets to false after a successful AckPacket
+		System.out.println("Content Length:" + fileContent.length + "\nbuffer Length: " + packetSize); // TODO: DEBUG STATEMENT DELETE AFTER
+		while (true) {
+			if (seqnoCounter > Math.ceil(fileContent.length / packetSize)) { //Check if we sent all necessary packets
+				break;
+			}
+			if (fileContent.length - startOffset < packetSize) { //Checks if we can still send our packetSize. If not, reset packetSize to remaining fileContent length
+				packetSize = fileContent.length - startOffset;
+			}
+			Packet dataPacket = createDataPacket(fileContent, startOffset, seqnoCounter, seqnoCounter, packetSize);
+			DatagramPacket requestPacket = new DatagramPacket(turnIntoByteArray(dataPacket), packetSize, inetAddress,
+					port);
+			int statusIdentifier = dataPacket.getStatus(corruptChance);
+
+			switch (statusIdentifier) { // Right now it is set to return 0 only -> default.
+			case (1): // TODO: Do nothing. Packet got dropped. Wait out timer and Resend same packet.
+			case (2): // TODO: Send packet but it got corrupted. Use method printCorruptStatus
+			default: // TODO: Send packet with no corruption.
+			}
+
+			long startTime = System.currentTimeMillis();
+			datagramSocket.send(requestPacket);
+			printSendStatus(requestPacket, statusIdentifier, startTime, timedOut);
+			startOffset += dataPacket.getData().length;
 			try {
-				Packet dataPacket = createDataPacket(fileContent, startOffset, seqnoCounter, seqnoCounter, packetSize); //TODO: Implement turning dataPacket into byte[] and into DatagramPacket
-				DatagramPacket requestPacket = new DatagramPacket(turnIntoByteArray(dataPacket), packetSize, inetAddress, port);
-				datagramSocket.send(requestPacket);
-				printToConsole(requestPacket);
-				startOffset += dataPacket.getData().length;
-				
-				//Create responsePacket and receive it from server. Not really needed for Project 1 but we will fix this for
-				//Project 2. Need to change it to an AckPacket. TODO: NEED DISCUSSION
+				datagramSocket.setSoTimeout(timeout);  //Sets timeout for receive() method. If timeout is reached, we continue with code.
 				DatagramPacket responsePacket = new DatagramPacket(new byte[packetSize], packetSize);
 				datagramSocket.receive(responsePacket);
 				if (checkAckPacket(responsePacket)) {
 					seqnoCounter++;
+					timedOut = false;
 				}
-				//TODO: Turn this into a method? Check the checksum of AckPacket and len == 8. If good, then check ackNo from packet against seqnoCounter.
+				else {
+					timedOut = true;
+				}
+			} catch (SocketException e) {
+				timedOut = true;
+				continue;
+			} 
+			// TODO: See if checkAckPacket method works before deleting.
 //				ByteBuffer bb = ByteBuffer.wrap(responsePacket.getData());
 //				byte[] checksumByte = new byte[2];
 //				byte[] lenByte = new byte[2];
@@ -112,29 +143,21 @@ public class Client {
 //						acknoCounter = ByteBuffer.wrap(acknoByte).getInt() + 1;
 //					}
 //				}
-			} catch (IOException e) {
-				e.printStackTrace();
-				break;
-			}
 		}
-		//Send last packet whose length < buffer.length and send flag packet TODO: Might change for loop to while loop so we can just put this in there.
-		int lastPackLen = fileContent.length - startOffset;
-		Packet lastPacket = createDataPacket(fileContent, startOffset, seqnoCounter, seqnoCounter, packetSize);
 		DatagramPacket flagPacket = new DatagramPacket(new byte[0], 0, inetAddress, port);
-		try {
-			DatagramPacket lastDGPacket = new DatagramPacket(turnIntoByteArray(lastPacket), lastPackLen, inetAddress, port);
-			printToConsole(lastDGPacket);
-			datagramSocket.send(lastDGPacket);		//Send last packet whose length < buffer.length
-			startOffset += lastPackLen;
-			System.out.println("Sending flag: " + flagPacket.getData() + " " + flagPacket.getLength()); //TODO: DEBUG STATEMENT DELETE AFTER
-			datagramSocket.send(flagPacket);	//Send an empty packet to denote no data left to send. (Our flag)
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		System.out.println("Sending empty packet: " + flagPacket.getLength()); //TODO: Debug print statement DELETE After
+		datagramSocket.send(flagPacket); // Send an empty packet to denote no data left to send. (Our flag)
 	}
 	
-	private boolean checkAckPacket(DatagramPacket ackPacket) {
+	/**
+	 * This method checks if the AckPacket we received from the Server/Receiver is not corrupted, has the correct length of 8, 
+	 * and makes sure the ackNo received is equal to the seqnoCounter+1 indicating that the server successfully received the correct
+	 * seqNo in the previous packet.
+	 * @param ackPacket: a DatagramPacket that contains the byte[] of data received from the Server/Receiver.
+	 * @return	true: iff the checkSum is 0, len is 8, and ackNo is seqnoCounter+1.
+	 * 			false: iff the checkSum is not 0 or len is not 8 or ackNo is not seqnoCounter+1. 
+	 */
+	private synchronized boolean checkAckPacket(DatagramPacket ackPacket) {
 		//TODO: Turn this into a method? Check the checksum of AckPacket and len == 8. If good, then check ackNo from packet against seqnoCounter.
 		ByteBuffer bb = ByteBuffer.wrap(ackPacket.getData());
 		byte[] checksumByte = new byte[2];
@@ -143,22 +166,39 @@ public class Client {
 		bb.get(lenByte, 0, lenByte.length);	//Gets the 3rd,4th bit then move buffer location to 4. (beginning of ackNo)
 		if (ByteBuffer.wrap(checksumByte).getInt() == 0 && ByteBuffer.wrap(lenByte).getInt() == 8) {
 			//Get ackNo from AckPacket. Check against seqNo. if Equal, increment by one both seqnoCounter and acknoCounter
-			byte[] acknoByte = new byte[4];
+			byte[] acknoByte = new byte[4]; //Last 4 bit are ackno
 			bb.get(acknoByte, 0, acknoByte.length);
-			if(ByteBuffer.wrap(acknoByte).getInt() == seqnoCounter && ByteBuffer.wrap(acknoByte).getInt() == acknoCounter) { //TODO: Check if this is correct from https://mkyong.com/java/java-convert-byte-to-int-and-vice-versa/
+			if(ByteBuffer.wrap(acknoByte).getInt() == seqnoCounter) { //TODO: Check if this is correct from https://mkyong.com/java/java-convert-byte-to-int-and-vice-versa/
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public Packet createDataPacket(byte[] file, int offset, int ackno, int seqno, int byteSize) {
-		Packet newPacket = new Packet(ackno, seqno, byteSize);
-		//TODO: STILL NEED TO IMPLEMENT WHERE WE SET DATA TO BYTE[] BEGINNING AT OFFSET
+	/**
+	 * Creates a Packet object using a file's byte[] beginning from an offset number.
+	 * @param file
+	 * @param offset
+	 * @param ackno
+	 * @param seqno
+	 * @param byteSize
+	 * @return
+	 */
+	private synchronized Packet createDataPacket(byte[] file, int offset, int ackno, int seqno, int byteSize) {
+		ByteBuffer bb = ByteBuffer.wrap(file);
+		byte[] data = new byte[byteSize];
+		bb.get(data, 0, data.length);
+		Packet newPacket = new Packet(ackno, seqno, data);
 		return newPacket;
 	}
 	
-	public byte[] turnIntoByteArray (Packet packet) throws IOException {
+	/**
+	 * Turns the Packet object into a byte[].
+	 * @param packet
+	 * @return
+	 * @throws IOException
+	 */
+	private byte[] turnIntoByteArray (Packet packet) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 	    ObjectOutputStream oos = new ObjectOutputStream(bos);
 	    oos.writeObject(packet);
@@ -168,19 +208,28 @@ public class Client {
 	}
 	
 	/**
-	 * Method is used to print to console what 
-	 * @param request
+	 * Method used to print to console, what occurred to the DatagramPacket.
+	 * @param request: the DatagramPacket whose information we are printing.
+	 * @param num: 
 	 */
-	public void printToConsole(DatagramPacket request) {
-		System.out.println(String.format("[Packet %d] - [start byte offset]: %d - [end byte offset]: %d", 
-				seqnoCounter, startOffset, startOffset+request.getLength()-1));
+	private void printSendStatus(DatagramPacket request, int statusN, long timerStartTime, boolean timedOutStatus) {
+		String packetStatus;
+		long timeToSend = System.currentTimeMillis() - timerStartTime;
+		switch(statusN){
+		case(1): packetStatus = "DROP";
+		case(2): packetStatus = "ERR";
+		default: packetStatus = "SENT";
+		}
+		String status = timedOutStatus ? "SENDing" : "ReSend";
+		System.out.println(String.format("%s %d %d : %d %d %s", 
+				status, seqnoCounter, startOffset, startOffset+request.getLength()-1, timeToSend, packetStatus));	//seqno, startOffset : endOffset, time sent in ms, status of packet (Sent, drop, error)
 	}
 	
 	// TODO: IMPLEMENT ON SERVER SIDE send the file name string to server there first. May not need
-	public void sendFileName() {
-		String fileName = outputFile.getName();
+	private void sendFileName(File file) {
+		String fileName = file.getName();
 		DatagramPacket sendFileNamePacket = new DatagramPacket(fileName.getBytes(), fileName.getBytes().length,
-				inetAddress, port);
+				inetAddress, DEFAULT_PORT);
 		try {
 			datagramSocket.send(sendFileNamePacket);
 		} catch (IOException e) {
@@ -193,9 +242,9 @@ public class Client {
 	/**
 	 * Main method. Executes the rest of the program when user inputs the file from cmd line.
 	 * @param args
-	 * @throws UnknownHostException
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws IOException {
 		int packetSize = -1;
 		int timeout = -1;
 		double corruptchance = -1;
@@ -210,6 +259,7 @@ public class Client {
 					System.out.println("Invalid packet length specified.");
 					return;
 				}
+				//TODO: Make an if statement to make sure packet length is between 0-500. Specified in documents. Throw exception if outside range.
 				i += 2;
 			} else if (args[i].equals("-t")) {
 				try {
@@ -236,13 +286,9 @@ public class Client {
 			}
 		}
 		
-		try (DatagramSocket datagramSocket = new DatagramSocket(0)) {
-			Client sender = new Client(datagramSocket, inetAddress, packetSize, timeout, corruptchance, port);
+			Client sender = new Client(inetAddress, packetSize, timeout, corruptchance, port);
 			sender.setFileContent(args[i]);
 			sender.sendPacket();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
 	}
 	
 }
