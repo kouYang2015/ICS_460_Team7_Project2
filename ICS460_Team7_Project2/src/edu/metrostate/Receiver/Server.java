9738c5c2a5c78234efe7e4fd57f0d49ef07372e5
Kou <Kou@KY>
2021-12-02 14:51:11 -0600

    Helper method sendAckPacket created and tested

--- ICS460_Team7_Project2/src/edu/metrostate/Receiver/Server.java
+++ ICS460_Team7_Project2/src/edu/metrostate/Receiver/Server.java
@@ -26,7 +26,7 @@ public class Server {
 	private int ackNo = 1;
 	private File fileReceived;
 	private double corruptChance;
-	
+
 	public Server(double corruptchance, InetAddress inetAddress, int port) throws SocketException {
 		super();
 		try {
@@ -40,84 +40,96 @@ public class Server {
 		System.out.println(this.inetAddress.getHostName());
 		System.out.println(this.port);
 	}
-	
+
 	public void receivePacket() throws ClassNotFoundException {
 		int checkSum;
 		int seqnoRec;
 		long startTime = System.currentTimeMillis();
-		while(true) {
+		while (true) {
 			try {
-				//Receive request and create a DatagramPacket. Then write it to file.
+				// Receive request and create a DatagramPacket. Then write it to file.
 				DatagramPacket requestPacket = new DatagramPacket(buffer, buffer.length);
 				datagramSocket.receive(requestPacket);
 				if (requestPacket.getLength() == 0) {
-					System.out.println("Flag packet:" + requestPacket.getData() + " " + requestPacket.getLength()); //TODO: DEBUG STATEMENT DELETE AFTER
+					System.out.println("Flag packet:" + requestPacket.getData() + " " + requestPacket.getLength()); //TODO:Delete
 					break;
 				}
 				Packet newPacket = deserializeByteArray(requestPacket);
 				checkSum = newPacket.getCksum();
 				seqnoRec = newPacket.getSeqno();
 				DatagramPacket responsePacket;
-//				System.out.println(checkSum);
-				System.out.println("len is " + newPacket.getLen());
+//				System.out.println(checkSum); //TODO:Delete
+//				System.out.println("len is " + newPacket.getLen());
 //				System.out.println(sentAckNoInt);
 //				System.out.println(deserializeByteArray(requestPacket).getSeqno());
-				System.out.println("data length is " + newPacket.getData().length);
-				if (checkSum == 1 || newPacket.getLen() != (newPacket.getData().length + 12)) { //if requestPacket is corrupted
+//				System.out.println("data length is " + newPacket.getData().length);
+				if (checkSum == 1 || newPacket.getLen() != (newPacket.getData().length + 12)) { 
+					//DataPacket is corrupted. Don't create and send AckPacket back.
 					printDataPacket(startTime, 2, seqnoRec);
-					//Don't create and send AckPacket back.
-				} else if (seqnoRec < ackNo) { //Got a non corrupted duplicate seqNo packet.
+				} else if (seqnoRec < ackNo) { 
+					// Got a non corrupted duplicate seqNo packet.
 					printDataPacket(startTime, 1, seqnoRec);
-					Packet dataPacket = createAckPacket(seqnoRec); //Create dataPacket with ackNo == seqNo we received
 					
-					int statusIdentifier = dataPacket.getStatus(corruptChance);
-					if (statusIdentifier == 1) {
-						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); 
-						continue;
-					}
-					else {
-						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
-					}
-//					switch (statusIdentifier) {
-//					case (1): printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); continue; //got dropped. dont send
-//					case (2): printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); //Corrupted but still send. no continue
-//					default: printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); //good ack packet. 
+					//Resend AckPacket with seqnoRec to inform Client to increment seqno.
+					sendAckPacket(requestPacket, seqnoRec, startTime);
+//					Packet dataPacket = createAckPacket(seqnoRec);
+//					
+//					int statusIdentifier = dataPacket.getStatus(corruptChance);
+//					if (statusIdentifier == 1) {
+//						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
+//						continue;
+//					} else {
+//						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
 //					}
-					responsePacket = new DatagramPacket(serializeByteArray(dataPacket), serializeByteArray(dataPacket).length,
-							requestPacket.getAddress(), requestPacket.getPort());
-					datagramSocket.send(responsePacket);
-				} else { //got new good packet. increment ackNo by one. so we can check if next packet is dupe (ackNo > seqNoReceived)
+//					responsePacket = new DatagramPacket(serializeByteArray(dataPacket),
+//							serializeByteArray(dataPacket).length, requestPacket.getAddress(), requestPacket.getPort());
+//					datagramSocket.send(responsePacket);
+				} else { // got new good packet. increment ackNo by one. so we can check if next packet
+							// is dupe (ackNo > seqNoReceived)
 					printDataPacket(startTime, 0, seqnoRec);
-					if (checkSum == 1 && newPacket.getLen() == (newPacket.getData().length + 12) && 
-							seqnoRec == ackNo) {
+					if (checkSum == 0 && newPacket.getLen() == (newPacket.getData().length + 12) && seqnoRec == ackNo) {
 						System.out.println("Good packet received");
 					}
-					Packet dataPacket = createAckPacket(seqnoRec); //Create dataPacket with ackNo == seqNo we received to acknowledge pack on client side
+					Packet dataPacket = createAckPacket(seqnoRec); // Create dataPacket with ackNo == seqNo we received
+																	// to acknowledge pack on client side
 					writeToFile(fileReceived, newPacket.getData());
-					int statusIdentifier = dataPacket.getStatus(corruptChance);
-					ackNo++; //Increment ackNo no matter what before doing switch case.
-					if (statusIdentifier == 1) {
-						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); 
-						continue;//got dropped. dont send
-					}
-					else {
-						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); //Corrupted or good still send. no continue
-					}
-//					switch (statusIdentifier) {
-//					case (1): printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); continue; //got dropped. dont send
-//					case (2): printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); //Corrupted but still send. no continue
-//					default: printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); //good ack packet. 
+					ackNo++; // Increment ackNo no matter what before doing switch case.
+					
+					//Send AckPacket.
+					sendAckPacket(requestPacket, seqnoRec, startTime);
+//					int statusIdentifier = dataPacket.getStatus(corruptChance);
+//					
+//					if (statusIdentifier == 1) {
+//						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
+//						continue;// got dropped. dont send
+//					} else {
+//						printAckStatus(startTime, statusIdentifier, dataPacket.getAckno()); // Corrupted or good still
+//																							// send. no continue
 //					}
-					responsePacket = new DatagramPacket(serializeByteArray(dataPacket), serializeByteArray(dataPacket).length,
-							requestPacket.getAddress(), requestPacket.getPort());
-					datagramSocket.send(responsePacket);
+//					responsePacket = new DatagramPacket(serializeByteArray(dataPacket),
+//							serializeByteArray(dataPacket).length, requestPacket.getAddress(), requestPacket.getPort());
+//					datagramSocket.send(responsePacket);
 				}
-				
 			} catch (IOException e) {
 				e.printStackTrace();
 				break;
 			}
 		}
+	}
+
+	private synchronized void sendAckPacket(DatagramPacket requestPacket, int seqnoRec, long startTime) throws IOException {
+		Packet dataPacket = createAckPacket(seqnoRec);
+		
+		int statusIdentifier = dataPacket.getStatus(corruptChance);
+		if (statusIdentifier == 1) {
+			printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
+			return;
+		} else {
+			printAckStatus(startTime, statusIdentifier, dataPacket.getAckno());
+		}
+		DatagramPacket responsePacket = new DatagramPacket(serializeByteArray(dataPacket),
+				serializeByteArray(dataPacket).length, requestPacket.getAddress(), requestPacket.getPort());
+		datagramSocket.send(responsePacket);
 	}
 	
 	/**
@@ -131,14 +143,14 @@ public class Server {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//byte[] to base64 string
+		// byte[] to base64 string
 		String encodedB64FileName = Base64.getEncoder().encodeToString(fileNamePacket.getData());
-        // base64 string to byte[]
-        byte[] decodeFileName = Base64.getDecoder().decode(encodedB64FileName);
-        String safeFileName = new String(decodeFileName); //Build string of decoded.
-		fileReceived = new File("image.jpg");	//Sets the new file to String of our decoded byte[]
+		// base64 string to byte[]
+		byte[] decodeFileName = Base64.getDecoder().decode(encodedB64FileName);
+		String safeFileName = new String(decodeFileName); // Build string of decoded.
+		fileReceived = new File("image.jpg"); // Sets the new file to String of our decoded byte[]
 	}
-	
+
 	/**
 	 * 
 	 * @param dp
@@ -146,33 +158,35 @@ public class Server {
 	 * @throws IOException
 	 * @throws ClassNotFoundException
 	 */
-	private synchronized Packet deserializeByteArray (DatagramPacket dp) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream bis = new ByteArrayInputStream(dp.getData());
-        ObjectInputStream ois = new ObjectInputStream(bis);
-        Packet deserializedPacket = (Packet) ois.readObject();
-        return deserializedPacket;
-    }
-	
+	private synchronized Packet deserializeByteArray(DatagramPacket dp) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(dp.getData());
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		Packet deserializedPacket = (Packet) ois.readObject();
+		return deserializedPacket;
+	}
+
 	private synchronized byte[] serializeByteArray(Packet packet) throws IOException {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	    ObjectOutputStream oos = new ObjectOutputStream(bos);
-	    oos.writeObject(packet);
-	    oos.flush();
-	    byte [] dataWithHeader = bos.toByteArray();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(packet);
+		oos.flush();
+		byte[] dataWithHeader = bos.toByteArray();
 		return dataWithHeader;
 	}
-	
+
 	/**
-	 * Creates a Packet that represents an ackPacket. 
+	 * Creates a Packet that represents an ackPacket.
+	 * 
 	 * @return Packet: The Packet object that represents an ackPacket.
 	 */
 	private synchronized Packet createAckPacket(int ackNo) {
 		Packet newPacket = new Packet(ackNo);
 		return newPacket;
 	}
-	
+
 	/**
 	 * Method used to concatenate new DataPacket byte[] to the file.
+	 * 
 	 * @param file: The file we want to add the new byte[] array to.
 	 * @param data: the byte[] data we will write to the file.
 	 */
@@ -185,7 +199,7 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private synchronized void printDataPacket(long startTimer, int receivedCode, int seqnoRec) {
 		String firstCode = "", errorCode = "";
 		long timeReceived = System.currentTimeMillis() - startTimer;
@@ -193,34 +207,29 @@ public class Server {
 		if (receivedCode == 1) {
 			firstCode += "DUPL";
 			errorCode += "!Seq";
-		}
-		else if (receivedCode == 2) {
+		} else if (receivedCode == 2) {
 			firstCode += "RECV";
 			errorCode += "CRPT";
-		}
-		else {
+		} else {
 			firstCode += "RECV";
 			errorCode += "RECV";
 		}
-		System.out.println(String.format("%s %d %d %s", 
-				firstCode, timeReceived, seqnoRec, errorCode));
+		System.out.println(String.format("%s %d %d %s", firstCode, timeReceived, seqnoRec, errorCode));
 	}
-	
+
 	private synchronized void printAckStatus(long startTimer, int sentCode, int ackPackNo) {
 		String errorCode = "";
 		long timeSent = System.currentTimeMillis() - startTimer;
 		if (sentCode == 1) {
 			errorCode += "DROP";
-		}
-		else if (sentCode == 2) {
+		} else if (sentCode == 2) {
 			errorCode += "ERR";
-		}
-		else {
+		} else {
 			errorCode += "SENT";
 		}
 		System.out.println(String.format("%s %s %d %d %s", "SENDing", "ACK", ackPackNo, timeSent, errorCode));
 	}
-	
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		double corruptchance = -1;
 		InetAddress inetAddress = null;
@@ -244,10 +253,10 @@ public class Server {
 				i++;
 			}
 		}
-		
+
 		Server receiver = new Server(corruptchance, inetAddress, port);
 		receiver.createFile();
 		receiver.receivePacket();
 	}
-	
+
 }
